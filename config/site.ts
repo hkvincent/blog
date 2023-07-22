ed309d6adaf051946a14f5b95f1ca0ddecd3f03b
vincent <hkvincentchan@gmail.com>
2023-07-22 20:33:54 +0800

    site info

---
+++ config/site.ts
@@ -0,0 +1,28 @@
+export interface SiteConfig {
+  siteName: string;
+  description: string;
+  currentlyAt: string;
+  socialLinks: {
+    twitter: string;
+    youtube: string;
+    github: string;
+    linkedin: string;
+    instagram: string;
+  };
+}
+
+const siteConfig: SiteConfig = {
+  siteName: "Explorer",
+  description:
+    "A minimal and lovely travel blog which shares experiences and citiest around the world!",
+  currentlyAt: "Budapest",
+  socialLinks: {
+    twitter: "https://github.com/hkvincent",
+    youtube: "https://github.com/hkvincent",
+    github: "https://github.com/hkvincent",
+    linkedin: "https://github.com/hkvincent",
+    instagram: "https://github.com/hkvincent",
+  },
+};
+
+export default siteConfig;
